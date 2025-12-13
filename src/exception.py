346ec00496fc683145b2543b948feb403adb73b6
Shuchi <goelshuchi273@gmail.com>
2025-12-13 12:55:54 +0530

    Logging and Exception

--- src/exception.py
+++ src/exception.py
@@ -23,11 +23,12 @@ class customException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__=="__main__":
+    
+# if __name__=="__main__":
 
-    try:
-        a = 1/0
+#     try:
+#         a = 1/0
 
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise customException(e, sys)
+#     except Exception as e:
+#         logging.info("Divide by zero")
+#         raise customException(e, sys)
